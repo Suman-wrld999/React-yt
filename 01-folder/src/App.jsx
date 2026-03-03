a531b85d48c5d94b0053d038d0ef904cdaf4c68c
Suman Patra <looptatake@gmail.com>
2026-03-04 00:01:28 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <>
+      <div id='dad'>
+        <h1>Heading 1</h1>
+        <h1>Heading 2</h1>
+      </div>
+
+      <div id='chacha'></div>
+      </>
+  )
+}
+
+export default App
